c513430668b24e3d4e0161617b74f6e4a60e3b6d
Victor Manuel Delfin Santos <victormanu31611@gmail.com>
2022-06-16 17:04:50 -0500

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     extend: {
       colors: {
         "my-blue-primary": "#2323D5",
+        "my-blue-primary-hover": "#4242ed",
         "my-green-primary": "#68B865",
       },
       fontSize: {
