fcd6cb4cbd187c9d6eda3cd3eeb0b421f5ed6ac7
VictorDelfin <victormanu31611@gmail.com>
2023-06-28 12:01:31 -0600

    new Privacy Policy text

--- src/Modules/Main/Terms.vue
+++ src/Modules/Main/Terms.vue
@@ -1,28 +1,29 @@
 <template>
   <div class="my-container mb-40 md:mb-0">
-    <h2 class="my-title-2 text-center md:text-left mb-8">
+    <h2 class="my-title-2 text-center md:text-left mb-4">
       {{ $t("terms.title") }}
     </h2>
 
-    <p class="mb-4">{{ $t("terms.gestsTitle") }}</p>
-    <p>{{ $t("terms.gestDescription") }}</p>
-
-    <p class="my-4">{{ $t("terms.airbnbTitle") }}</p>
-    <p>{{ $t("terms.airbnbDescription") }}</p>
-
-    <p class="my-8">{{ $t("terms.airbnbDescription2") }}</p>
-
-    <p class="my-8">{{ $t("terms.airbnbDescription3") }}</p>
-
-    <p class="my-8">{{ $t("terms.airbnbDescription4") }}</p>
+    <p class="mb-4">{{ $t("terms.parragraph_0") }}</p>
+    <p class="font-semibold">{{ $t("terms.parragraph_1") }}</p>
+    <p class="mb-4">{{ $t("terms.parragraph_1_1") }}</p>
+    <p class="font-semibold">{{ $t("terms.parragraph_2") }}</p>
+    <p class="mb-4">{{ $t("terms.parragraph_2_1") }}</p>
+    <p class="font-semibold">{{ $t("terms.parragraph_3") }}</p>
+    <p class="mb-4">{{ $t("terms.parragraph_3_1") }}</p>
+    <p class="font-semibold">{{ $t("terms.parragraph_4") }}</p>
+    <p class="mb-4">{{ $t("terms.parragraph_4_1") }}</p>
+    <p class="font-semibold">{{ $t("terms.parragraph_5") }}</p>
+    <p class="mb-4">{{ $t("terms.parragraph_5_1") }}</p>
+    <p class="font-semibold">{{ $t("terms.parragraph_6") }}</p>
+    <p class="mb-4">{{ $t("terms.parragraph_6_1") }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  metaInfo: {title: "Términos y condiciones"},
+  metaInfo: { title: "Términos y condiciones" },
 };
-
 </script>
 
 <style></style>
